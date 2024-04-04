ee7778366ec5357200b713fcecbef0afe23f8542
tejasbeehyv <121476648+tejasbeehyv@users.noreply.github.com>
2024-04-04 13:22:08 +0530

    Update main.tf

--- infra-as-code/terraform/azure-pucar/main.tf
+++ infra-as-code/terraform/azure-pucar/main.tf
@@ -1,4 +1,8 @@
 provider "azurerm" {
+  subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
   features {}
   skip_provider_registration = true
 }
